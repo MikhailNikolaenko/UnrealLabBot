dfc54fcaff3244b8eb01e8c0e853ea8db093498b
Mikhail <nikolamv@mail.uc.edu>
2024-05-28 18:39:47 -0400

    ini
    websocket node code

---
+++ Source/LabBot/PythonCommunicator.cpp
@@ -0,0 +1,212 @@
+#include "PythonCommunicator.h"
+#include "Engine/Engine.h"
+#include "Json.h"
+#include "JsonUtilities.h"
+#include "Sound/SoundWave.h"
+#include "AudioDecompress.h"
+#include "AudioDevice.h"
+#include "Misc/FileHelper.h"
+#include "HAL/PlatformFilemanager.h"
+#include "AudioDevice.h"
+
+// Sets default values
+APythonCommunicator::APythonCommunicator()
+{
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void APythonCommunicator::BeginPlay()
+{
+    Super::BeginPlay();
+    StartWebSocketServer();
+}
+
+// Called every frame
+void APythonCommunicator::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+// Called when the game ends or when the actor is destroyed
+void APythonCommunicator::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+    Super::EndPlay(EndPlayReason);
+    StopWebSocketServer();
+
+    //// Wait until the WebSocket is closed
+    //while (!bWebSocketClosed)
+    //{
+    //    FPlatformProcess::Sleep(0.1f);
+    //}
+}
+
+void APythonCommunicator::StartWebSocketServer()
+{
+    if (!FModuleManager::Get().IsModuleLoaded("WebSockets"))
+    {
+        FModuleManager::Get().LoadModule("WebSockets");
+    }
+
+    UE_LOG(LogTemp, Log, TEXT("Starting WebSocket..."));
+    WebSocket = FWebSocketsModule::Get().CreateWebSocket(TEXT("ws://localhost:6789"));
+
+    if (!WebSocket.IsValid())
+    {
+        UE_LOG(LogTemp, Error, TEXT("Failed to create WebSocket"));
+        return;
+    }
+
+    WebSocket->OnConnected().AddUObject(this, &APythonCommunicator::OnWebSocketConnected);
+    WebSocket->OnConnectionError().AddUObject(this, &APythonCommunicator::OnWebSocketConnectionError);
+    WebSocket->OnClosed().AddUObject(this, &APythonCommunicator::OnWebSocketClosed);
+    WebSocket->OnMessage().AddUObject(this, &APythonCommunicator::OnWebSocketMessageReceived);
+
+    WebSocket->Connect();
+}
+
+void APythonCommunicator::StopWebSocketServer()
+{
+    if (WebSocket.IsValid())
+    {
+        UE_LOG(LogTemp, Log, TEXT("Closing WebSocket..."));
+        WebSocket->Close();
+    }
+}
+
+void APythonCommunicator::OnWebSocketConnected()
+{
+    UE_LOG(LogTemp, Log, TEXT("WebSocket connected"));
+    // Send initial message to the server to register the connection
+    FString initialMessage = TEXT("{\"name\": \"UnrealClient\"}");
+    WebSocket->Send(initialMessage);
+}
+
+void APythonCommunicator::OnWebSocketConnectionError(const FString& Error)
+{
+    UE_LOG(LogTemp, Error, TEXT("WebSocket connection error: %s"), *Error);
+    bWebSocketClosed = true;
+}
+
+void APythonCommunicator::OnWebSocketClosed(int32 StatusCode, const FString& Reason, bool bWasClean)
+{
+    UE_LOG(LogTemp, Log, TEXT("WebSocket closed: %s"), *Reason);
+    bWebSocketClosed = true;
+
+}
+
+void APythonCommunicator::OnWebSocketMessageReceived(const FString& Message)
+{
+    UE_LOG(LogTemp, Log, TEXT("Received message: %s"), *Message);
+
+    TSharedPtr<FJsonObject> JsonObject;
+    TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(Message);
+
+    if (FJsonSerializer::Deserialize(Reader, JsonObject) && JsonObject.IsValid())
+    {
+        FResponseResult ResponseData;
+
+        FString RequestType = JsonObject->GetStringField("type");
+        if (RequestType == "audio")
+        {
+            FString AudioData = JsonObject->GetStringField("data");
+            TArray<uint8> AudioBytes;
+            FBase64::Decode(AudioData, AudioBytes);
+            AccumulatedAudio.Append(AudioBytes);
+        }
+        else if (RequestType == "audio_end")
+        {
+            // Create a SoundWave from the accumulated audio bytes
+            USoundWave* SoundWave = CreateSoundWaveFromBytes(AccumulatedAudio);
+            if (SoundWave)
+            {
+                ResponseData.SoundWave = SoundWave;
+                UE_LOG(LogTemp, Log, TEXT("SoundWave created successfully"));
+            }
+            else
+            {
+                UE_LOG(LogTemp, Error, TEXT("Failed to create SoundWave"));
+            }
+
+            ResponseData.audio = AccumulatedAudio;
+            OnResponseReady.Broadcast(ResponseData);
+            AccumulatedAudio.Empty();  // Clear the accumulated data for the next audio
+        }
+        else if (RequestType == "text")
+        {
+            ResponseData.text = JsonObject->GetStringField("data");
+            OnResponseReady.Broadcast(ResponseData);
+        }
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("Failed to parse JSON message"));
+    }
+}
+
+bool APythonCommunicator::FillSoundWave(USoundWave* SoundWave, const TArray<uint8>& RawFileData)
+{
+    if (!SoundWave || RawFileData.Num() == 0)
+    {
+        UE_LOG(LogTemp, Error, TEXT("Invalid SoundWave pointer or empty RawFileData"));
+        return false;
+    }
+
+    // Print out the first few bytes of the RawFileData
+    FString FirstBytes;
+    for (int32 i = 0; i < FMath::Min(RawFileData.Num(), 64); ++i)
+    {
+        FirstBytes += FString::Printf(TEXT("%02x "), RawFileData[i]);
+    }
+    UE_LOG(LogTemp, Log, TEXT("First bytes of RawFileData: %s"), *FirstBytes);
+
+    FWaveModInfo WaveInfo;
+    FString ErrorReason;
+    if (!WaveInfo.ReadWaveInfo(RawFileData.GetData(), RawFileData.Num(), &ErrorReason))
+    {
+        UE_LOG(LogTemp, Error, TEXT("Failed to read wave info: %s. Data size: %d"), *ErrorReason, RawFileData.Num());
+        return false;
+    }
+
+    if (!WaveInfo.SampleDataStart || !WaveInfo.pChannels || !WaveInfo.pSamplesPerSec)
+    {
+        UE_LOG(LogTemp, Error, TEXT("Invalid wave info data pointers"));
+        return false;
+    }
+
+    SoundWave->RawPCMDataSize = WaveInfo.SampleDataSize;
+    SoundWave->RawPCMData = static_cast<uint8*>(FMemory::Malloc(SoundWave->RawPCMDataSize));
+    if (!SoundWave->RawPCMData)
+    {
+        UE_LOG(LogTemp, Error, TEXT("Failed to allocate memory for PCM data"));
+        return false;
+    }
+
+    FMemory::Memcpy(SoundWave->RawPCMData, WaveInfo.SampleDataStart, SoundWave->RawPCMDataSize);
+
+    SoundWave->Duration = static_cast<float>(SoundWave->RawPCMDataSize) / (*WaveInfo.pChannels * *WaveInfo.pSamplesPerSec * sizeof(int16));
+    SoundWave->NumChannels = *WaveInfo.pChannels;
+    SoundWave->SetSampleRate(*WaveInfo.pSamplesPerSec);
+
+    UE_LOG(LogTemp, Log, TEXT("Successfully filled SoundWave with WAV data"));
+    return true;
+}
+
+USoundWave* APythonCommunicator::CreateSoundWaveFromBytes(const TArray<uint8>& AudioBytes)
+{
+    USoundWave* SoundWave = NewObject<USoundWave>(USoundWave::StaticClass());
+    if (!SoundWave)
+    {
+        UE_LOG(LogTemp, Error, TEXT("Failed to create SoundWave"));
+        return nullptr;
+    }
+
+    if (!FillSoundWave(SoundWave, AudioBytes))
+    {
+        UE_LOG(LogTemp, Error, TEXT("Failed to fill SoundWave with data"));
+        return nullptr;
+    }
+
+    return SoundWave;
+}
---
+++ Source/LabBot/PythonCommunicator.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "IWebSocket.h"
+#include "WebSocketsModule.h"
+#include "Sound/SoundWave.h"
+#include "PythonCommunicator.generated.h"
+
+USTRUCT(BlueprintType)
+struct FResponseResult
+{
+    GENERATED_BODY()
+
+    UPROPERTY(BlueprintReadOnly)
+    FString text;
+
+    UPROPERTY(BlueprintReadOnly)
+    TArray<uint8> audio;
+
+    UPROPERTY(BlueprintReadOnly)
+    USoundWave* SoundWave;  // Add SoundWave pointer
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnResponseReady, const FResponseResult&, ResponseData);
+
+UCLASS()
+class LABBOT_API APythonCommunicator : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this actor's properties
+    APythonCommunicator();
+
+protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+    // Called when the game ends or the actor is destroyed
+    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    // Delegate to be called when we receive machine data
+    UPROPERTY(BlueprintAssignable, Category = "Web")
+    FOnResponseReady OnResponseReady;
+
+private:
+    TSharedPtr<IWebSocket> WebSocket;
+    bool bWebSocketClosed;
+    TArray<uint8> AccumulatedAudio;  // Variable to store accumulated audio data
+
+    void StartWebSocketServer();
+    void StopWebSocketServer();
+    void OnWebSocketConnected();
+    void OnWebSocketMessageReceived(const FString& Message);
+    bool FillSoundWave(USoundWave* SoundWave, const TArray<uint8>& RawFileData);
+    void OnWebSocketConnectionError(const FString& Error);
+    void OnWebSocketClosed(int32 StatusCode, const FString& Reason, bool bWasClean);
+
+    USoundWave* CreateSoundWaveFromBytes(const TArray<uint8>& AudioBytes);  // Function to create SoundWave from WAV file
+
+};
